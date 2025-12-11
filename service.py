fd2de088973e3e2a916167efd525525df1285105
Bulbanator123 <lebedevdanka4@gmail.com>
2025-12-11 23:03:16 +0300

    change ocr

--- service.py
+++ service.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
-from PIL import Image
 import cv2
-import easyocr
+from fast_plate_ocr import LicensePlateRecognizer
 import os
 import numpy as np
 
@@ -10,30 +9,11 @@ model_lisence_plates = YOLO('model/yolo_lisence_plate.pt')
 model_vehicle = YOLO('model/yolov8m.pt')
 vehicles = [2, 3, 5, 7]
 
-reader = easyocr.Reader(["en"], gpu=True)
+reader = LicensePlateRecognizer("cct-xs-v1-global-model")
 
 HOME = os.getcwd()  # Getting the current working directory
 category_img = "images"
 category_save = "saves"
-
-
-def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=200, line_length_y=200):
-    x1, y1 = top_left
-    x2, y2 = bottom_right
-
-    cv2.line(img, (x1, y1), (x1, y1 + line_length_y), color, thickness)  #-- top-left
-    cv2.line(img, (x1, y1), (x1 + line_length_x, y1), color, thickness)
-
-    cv2.line(img, (x1, y2), (x1, y2 - line_length_y), color, thickness)  #-- bottom-left
-    cv2.line(img, (x1, y2), (x1 + line_length_x, y2), color, thickness)
-
-    cv2.line(img, (x2, y1), (x2 - line_length_x, y1), color, thickness)  #-- top-right
-    cv2.line(img, (x2, y1), (x2, y1 + line_length_y), color, thickness)
-
-    cv2.line(img, (x2, y2), (x2, y2 - line_length_y), color, thickness)  #-- bottom-right
-    cv2.line(img, (x2, y2), (x2 - line_length_x, y2), color, thickness)
-
-    return img
 
 
 def post_proccesing_image(image):
@@ -81,16 +61,13 @@ def recognition_vehicles(image):
 
 
 def ocr_detections(lisence_crop_img):
-    text = ""
-    lisence_detection = reader.readtext(lisence_crop_img, width_ths=0.1)
-    for lisence_text in lisence_detection:
-        text += lisence_text[1]
-    return text
+    lisence_detection = reader.run(lisence_crop_img)
+    return lisence_detection
 
 
 def recognition_lisence_plate(images):
+    detections = []
     for vehicle_crop_img in images:
-        detections = []
             # Run the YOLO model on the current vehicle image
         results_lisence = model_lisence_plates(vehicle_crop_img)[0]
 
@@ -102,14 +79,13 @@ def recognition_lisence_plate(images):
 
             if conf < 0.7:
                 continue
-            lisence_crop_img = vehicle_crop_img[int(y1):int(y2), int(x1):int(x2)] 
-            lisence_crop_img = post_proccesing_image(lisence_crop_img)
-
+            lisence_crop_img = vehicle_crop_img[int(y1):int(y2), int(x1):int(x2)]
+            # lisence_crop_img = post_proccesing_image(lisence_crop_img)
             # Now apply the OCR on the processed image
             det_text = ocr_detections(lisence_crop_img)
-            if len(det_text) > 0:
-                detections.append([det_text, (x1, x2, y1, y2)])
-                save_lp(lisence_crop_img)
+            detections.append([*det_text, (x1, x2, y1, y2)])
+            print(1)
+            save_lp(lisence_crop_img)
             # cv2.imshow('cropped', lisence_crop_img)
             # cv2.waitKey(0)
 
@@ -131,7 +107,7 @@ def detect_lisence_plates_in_folder(images_folder):
                 continue
 
             image = cv2.imread(file_path)
-            detections.extend([recognition_lisence_plate(recognition_vehicles(image=image))])
+            detections.extend(recognition_lisence_plate(recognition_vehicles(image=image)))
 
         if file_name.endswith((".mp4", ".mov", ".avi", ".webm", ".giff")):
             # Check videos that we get
@@ -139,9 +115,9 @@ def detect_lisence_plates_in_folder(images_folder):
             if type(file_path) is None:
                 continue
 
-            frame_num = -1
+            frame_num = -1              
             ret = True
-            cap = cv2.VideoCapture('videos/sample.mp4')
+            cap = cv2.VideoCapture(file_path)
             while ret:
                 frame_num += 1
                 ret, frame = cap.read()
@@ -151,17 +127,6 @@ def detect_lisence_plates_in_folder(images_folder):
                     detections.extend(recognition_lisence_plate(recognition_vehicles(image=frame)))
                 
     return detections
-
-
-# def completed_img():
-#     bordered_img = draw_border(bordered_img, (int(carx1), int(cary1)), (int(carx2), int(cary2)), (0, 255, 0), 10)
-            
-#     (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.3, 5)
-
-#     cv2.putText(bordered_img, text, (int((carx2 + carx1 - text_width) / 2), int(cary1 + (text_height))),
-#         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 5)
-#     cv2.imshow("complete_img", bordered_img)
-#     cv2.waitKey(0)
 
 
 def main():
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-cv2.imshow('cropped', lisence_crop_img)
-            # cv2.waitKey(0)
+    # ocr_model="cct-xs-v1-global-model",
